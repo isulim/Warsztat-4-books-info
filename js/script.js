23d2c83eac3505fc5bab1f777a869b6ae2085758
isulim <ihor.sulym@gmail.com>
2019-02-16 16:02:39 +0100

    Zadanie 6 - usuwanie

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
 
     var list = $('#books-list ul');
 
-    var main = function () {
+    var loadAll = function() {
+        list.html('');
         $.ajax({
             url: url,
             data: {},
@@ -13,28 +14,44 @@ $(document).ready(function () {
         }).done(function (result) {
             for (let i = 0; i < result.length; i++) {
 
-                list.append('<li class="book-title" id="' + result[i].id + '">' + result[i].title + '<div></div></li>');
+                list.append('<li class="book-title" id="' + result[i].id + '">' + result[i].title +
+                    '<div></div></li><a href="#" class="delete-book">USUŃ</a>');
             }
         }).fail(function (xhr, status, err) {
             alert('Błąd\n' + xhr + status + err)
         });
 
-    list.on('click', 'li.book-title', function () {
-        let id = $(this).attr('id');
-        let desc = $(this).children(0);
-        desc.toggle();
-        $.ajax({
-            url: url + id,
-            data: {},
-            method: 'GET',
-            dataType: 'json',
-        }).done(function (result) {
-            desc.html(result.id + result.author + result.isbn + result.publisher);
+        list.on('click', 'li.book-title', function () {
+            let id = $(this).attr('id');
+            let desc = $(this).children(0);
+            desc.toggle();
+            $.ajax({
+                url: url + id,
+                data: {},
+                method: 'GET',
+                dataType: 'json',
+            }).done(function (result) {
+                desc.html(result.id + result.author + result.isbn + result.publisher);
+            });
         });
-    });
     };
 
-        $('#new-book').on('submit', function (event) {
+    list.on('click', 'a.delete-book', function () {
+        let id = $(this).prev().attr('id');
+
+        $.ajax({
+            url: url + id,
+            method: 'DELETE',
+            data: {},
+            dataType: 'json',
+        }).done(function (result) {
+            alert('Usunięto!');
+            loadAll();
+        })
+    });
+
+
+    $('#new-book').on('submit', function (event) {
         event.preventDefault();
 
         let data = {
@@ -54,12 +71,12 @@ $(document).ready(function () {
             method: 'POST',
             data: data,
             dataType: 'json',
-        }).done(function (result) {
+        }).done(function () {
             alert('DODANO!');
-            main();
+            loadAll();
         })
     });
 
-    main();
+    loadAll();
 
 });
