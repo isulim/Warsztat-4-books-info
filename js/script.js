8994d4e586a7646291053e28523f193b0fb4e150
isulim <ihor.sulym@gmail.com>
2019-02-19 20:41:26 +0100

    Bootstrap - styl, header, footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
 
+    $(window).scroll(function () {
+        var s = $(document).scrollTop(),
+            d = $(document).height() - $(window).height();
+        $("#progressbar").attr('aria-valuemax', d);
+        $("#progressbar").attr('aria-valuenow', s);
+     });
     var url = 'http://127.0.0.1:8000/book/';
 
     var list = $('#books-list');
@@ -13,59 +19,58 @@ $(document).ready(function () {
         }).done(ajaxDone).fail(function (xhr, status, err){
             alert('Błąd\n' + xhr + status + err)
         })
-    }
+    };
 
     
     var populateList = function(result) {
         
         for (let i = 0; i < result.length; i++) {
-            list.append('<li class="list-group-item " book-id="' + result[i].id + '">' + 
+            list.append('<li class="list-group-item list-group-item-action flex-column align-items-start" data-id="' + result[i].id + '">' +
                             '<div class="row">' + 
-                                '<div class="col title">' + 
+                                '<div class="col book-info ">' +
                                     result[i].title + 
                                 '</div>' +         
                                 '<div class="col">' + 
-                                    '<button class="btn btn-warning delete-book" delete-id="' + result[i].id + '">Usuń wpis</button>' + 
+                                    '<button class="btn btn-primary delete-book" data-id="' + result[i].id + '">Usuń wpis</button>' +
                                 '</div>' + 
                             '</div>' + 
-                            '<div class="col" style="display: none;" id="desc-' + result[i].id + '"></div>' +
+                            '<div class="col book-info" style="display: none;" id="desc-' + result[i].id + '"></div>' +
                         '</li>');
         } 
 
-        $('ol li').one('click', function (event) {
-            let id = $(this).attr('book-id');
+        $('ol li').one('click', function () {
+            let id = $(this).data('id');
             let desc = $('#desc-' + id);
 
             let bookDetails = function(result){
                 desc.html('<div class="container"><div class="row">Autor: ' + result.author + '</div></div>' +
                             '<div class="container"><div class="row">ISBN: ' + result.isbn + '</div></div>' +
-                            '<div class="container"><div class="row">Wydawca: ' + result.publisher + '</div></div>');            
-            }
+                            '<div class="container"><div class="row">Wydawca: ' + result.publisher + '</div></div>' +
+                            '<div class="container"><div class="row">Gatunek: ' + result.genre + '</div></div>');
+            };
             ajaxCall((url + id),'GET', {}, 'json', bookDetails);
            
             });
 
 
-            $('.title').on('click', function (){
-                let id = $(this).parent().parent().attr('book-id');
-                let desc = $('#desc-' + id);
-                desc.toggle();
-            })
+        $('.book-info').on('click', function (){
+            let id = $(this).closest('li').data('id');
+            let desc = $('#desc-' + id);
+            desc.toggle();
+        });
 
-            $('.delete-book').off();
-        
-            $('.delete-book').on('click', function () {
-                let id = $(this).attr('delete-id');
-                let deleteDone = function() {
-                    loadAll();
-                }
-                if (confirm('Czy na pewno usunąć wpis?')){
-                    ajaxCall((url + id), 'DELETE', {}, 'json', deleteDone)
-                }
-                
-        
-            });
-    }
+        $('.delete-book').off('click').on('click', function () {
+            let id = $(this).data('id');
+            let deleteDone = function() {
+                loadAll();
+            };
+            if (confirm('Czy na pewno usunąć wpis?')){
+                ajaxCall((url + id), 'DELETE', {}, 'json', deleteDone)
+            }
+
+
+        });
+    };
 
 
     var loadAll = function() {
@@ -86,12 +91,12 @@ $(document).ready(function () {
             genre: $(this).find('#genre').val(),
         };
         
-        let done = function() {
-            alert('Dodano nową książkę do bazy.')
+        let detailsDone = function() {
+            alert('Dodano nową książkę do bazy.');
             loadAll();
-        }
+        };
 
-        ajaxCall(url, 'POST', data, 'json', done)
+        ajaxCall(url, 'POST', data, 'json', detailsDone)
 
     });
 
